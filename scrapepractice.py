4cf1cbd851d2281cee8c5165ea09d8d4293e0a3c
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-03-04 08:45:35 -0300

    Update scrapepractice.py

--- scrapepractice.py
+++ scrapepractice.py
@@ -66,7 +66,6 @@ def extract_book_categories():
 
     bs = BeautifulSoup(html, 'html.parser')
 
-    # title, category, price, rating
     books = bs.find_all('article', {'class' : 'product_pod'})
 
     for book in books:
@@ -76,6 +75,8 @@ def extract_book_categories():
         book_price = book.find('div', {'class': 'product_price'}).find('p', {'class': 'price_color'}).get_text()
 
         book_rating = book.find('p', {'class': 'star-rating'})['class']
+
+        
 
         for r in book_rating:
             if r != 'star-rating':
@@ -90,17 +91,43 @@ def extract_book_categories():
         }
         actual_rating = rating_dictionary.get(actual_rating, "wrong")
 
-        print(actual_rating)
+        is_book_available = book.find('p', {'class': 'availability'})['class']
         
+        next_page = requests.get(book_url)
+        next_bs = BeautifulSoup(next_page.content, 'html.parser')
 
-        # print(book_url)
-        # print(f'TITLE: {book_title}')
-        # print(f'PRICE: {book_price}')
-        # print(book_rating)
+        category = next_bs.find("ul", {"class": "breadcrumb"}).find_all('li')[2:3]
+        actual_category = ''.join(str(x) for x in category)
+        match = re.search('<a\s+href="[^"]*">(.*?)<\/a>', actual_category)
+
+        # book image URL
+        book_image_url = next_bs.find('div', {'class': 'thumbnail'}).find('img')
+        actual_book_url = book_image_url.attrs['src']
+        print(f'https://books.toscrape.com/{actual_book_url}')
+
+        # number of reviews
+        number_of_reviews = next_bs.find('table', {'class': 'table-striped'}).find_all('tr')[-1].find('td').get_text()
+
+        print(f'TITLE: {book_title}')
+
+        if match:
+            actual_text = match.group(1)
+            print(f'CATEGORY: {actual_text}')
+        else:
+            print(actual_text)
+
+        print(f'PRICE: {book_price}')
+        print(f'RATING: {actual_rating}')
+        print(f'NUMBER OF REVIEWS: {number_of_reviews}')
+        if 'instock' in is_book_available:
+            print('Book Instock')
+        else:
+            print('book not available')
+                    
+        print(book_url)
 
         print("="*40) # separator
 
-    # to get the category we also need to extract the URL
         
 
 extract_book_categories()
